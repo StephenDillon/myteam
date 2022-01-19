5c30473e92a10bd7d86414dcbb67c66867762280
Stephen Dillon <yodareloaded@gmail.com>
2022-01-18 22:07:42 -0500

    Adding backend api app

---
+++ myteam-api/src/main/java/dillos/myteamapi/MyteamApiApplication.java
@@ -0,0 +1,13 @@
+package dillos.myteamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyteamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyteamApiApplication.class, args);
+	}
+
+}
